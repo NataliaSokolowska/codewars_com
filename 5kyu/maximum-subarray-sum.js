c1762f46e551d798a6e01a68d86a63d6e429b987
NataliaSokolowska <nataliasokolowska@bedeprogramistka.pl>
2023-01-03 11:29:38 +0100

    Use reduce method in maximum subarray sum

--- 5kyu/maximum-subarray-sum.js
+++ 5kyu/maximum-subarray-sum.js
@@ -9,17 +9,27 @@ Easy case is when the list is made up of only positive numbers and the maximum s
 Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 */
 
+//with reduce
 const maxSequence = (arr) => {
-  if (arr.length === 0) return 0;
+  let currentSum = 0;
 
-  let maxCurrent = arr[0];
-  let maxGlobal = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
-    if (maxCurrent > maxGlobal) {
-      maxGlobal = maxCurrent;
-    }
-  }
-  return maxGlobal;
+  return arr.reduce((maxSum, number) => {
+    currentSum = Math.max(currentSum + number, 0);
+    return Math.max(currentSum, maxSum);
+  }, 0);
 };
+
+// const maxSequence = (arr) => {
+//   if (arr.length === 0) return 0;
+
+//   let maxCurrent = arr[0];
+//   let maxGlobal = arr[0];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     maxCurrent = Math.max(arr[i], maxCurrent + arr[i]);
+//     if (maxCurrent > maxGlobal) {
+//       maxGlobal = maxCurrent;
+//     }
+//   }
+//   return maxGlobal;
+// };
